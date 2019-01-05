748382b2ce9103fda4ac0313e027af2694b641f6
Johansson <inste@icloud.com>
2019-01-05 13:12:49 +0200

    Initial Commit

---
+++ RandomNumber/main.m
@@ -0,0 +1,17 @@
+//
+//  main.m
+//  RandomNumber
+//
+//  Created by Artem Karmaz on 1/5/19.
+//  Copyright © 2019 Johansson Group. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+int main(int argc, const char * argv[]) {
+    @autoreleasepool {
+        // insert code here...
+        NSLog(@"Hello, World!");
+    }
+    return 0;
+}
